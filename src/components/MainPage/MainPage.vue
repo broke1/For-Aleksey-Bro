a842bde6a100bb5d0477f47609bc2b9533f984a4
broke1 <fredswork@list.ru>
2021-12-11 23:39:17 +0300

    add title

--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="main-container">
     <div class="container">
-      
+      <div class="title_on_main">
+        <div 
+          class="title"
+          :class="{title_show: title_show}"
+        >
+          Брат Алексея
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -14,17 +21,19 @@ import './MainPage.sass'
 export default {
   data () {
     return {
-    
+      title_show: false,
     }
   },
   methods: {
-    
+
   },
   created() {
 
   },
   mounted() {
-
+    setTimeout( () => {
+      this.title_show=true
+    },2000)
   }
 }
 </script>
